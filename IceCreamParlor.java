f437b539f699578edf85384d0c3d66f30bb64c4b
Waldir Miron Berbel <waldirmbf@gmail.com>
2020-05-10 10:36:26 -0500

    Amending comment

--- IceCreamParlor.java
+++ IceCreamParlor.java
@@ -1,7 +1,6 @@
 /*Sunny and Johnny like to pool their money and go to the ice cream parlor. 
 Johnny never buys the same flavor that Sunny does. The only other rule they 
 have is that they spend all of their money.
-
 Given a list of prices for the flavors of ice cream, select the two that will 
 cost all of the money they have.*/
 
